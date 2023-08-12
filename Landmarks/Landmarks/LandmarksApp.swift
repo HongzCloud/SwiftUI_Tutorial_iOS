61ba36f971a2e903862a2f55037d71e6cadad4e6
HongzCloud <jihong21511@gmail.com>
2023-08-13 01:53:39 +0900

    :tada: SwiftUI tutorial project 생성

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 오킹 on 2023/08/13.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
