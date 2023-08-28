34a6c46969e1d5268db8d5c4db335ca4ebc1d7c2
HongzCloud <jihong21511@gmail.com>
2023-08-29 00:55:53 +0900

    :lipstick: Section 2 - Create the View Controller’s Data Source

--- Landmarks/Landmarks/Views/Landmarks/PageView.swift
+++ Landmarks/Landmarks/Views/Landmarks/PageView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct PageView<Page: View>: View {
     var pages: [Page]
-
-
+    
     var body: some View {
         PageViewController(pages: pages)
     }
@@ -20,5 +19,5 @@ struct PageView<Page: View>: View {
 struct PageView_Previews: PreviewProvider {
     static var previews: some View {
         PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
-                   .aspectRatio(3 / 2, contentMode: .fit)    }
+        .aspectRatio(3 / 2, contentMode: .fit)    }
 }
